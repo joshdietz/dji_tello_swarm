243855cdbb947f76dd523e48123f1decad5d5a40
Josh Dietz <jdietz19@georgefox.edu>
2021-11-05 09:46:44 -0700

    added send/recieve methods for swarm control

--- swarm.py
+++ swarm.py
@@ -42,7 +42,7 @@ for device in DEVICES:
     mac_address = MAC_PREFIX + ":" + device[0:2] + ":" + device[2:4]
 
     DRONES.append({
-        "ID": device,
+        "ID": device.upper(),
         "MAC": mac_address,
         "IP":""
     })
@@ -74,23 +74,54 @@ END OF NETWORK SCANNING SECTION
 START OF SETUP SECTION
 """
 for drone in DRONES:
-    # setup tello and local addresses
-    tello = (drone["IP"], 8889)
-    local = ('', NEXT_AVAILABLE_PORT)
-    TELLO_ADDRESSES.append(tello)
-    LOCAL_ADDRESSES.append(local)
+    if drone["IP"] != "":
+        # setup tello and local addresses
+        tello = (drone["IP"], 8889)
+        local = ('', NEXT_AVAILABLE_PORT)
+        TELLO_ADDRESSES.append(tello)
+        LOCAL_ADDRESSES.append(local)
 
-    # increment next available port
-    NEXT_AVAILABLE_PORT += 1
+        # increment next available port
+        NEXT_AVAILABLE_PORT += 1
 
-    # setup sockets
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # setup sockets
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # bind the socket to the local address
-    sock.bind(local)
+        print("Binding drone: {}".format(drone["ID"]))
+        # bind the socket to the local address
+        sock.bind(local)
 
-    # add the socket to the list of sockets
-    SOCKETS.append(sock)
+        # add the socket to the list of sockets
+        SOCKETS.append(sock)
+
+def send(message, delay):
+    for i in len(TELLO_ADDRESSES):
+        socket = SOCKETS[i]
+        address = TELLO_ADDRESSES[i]
+        try:
+            socket.sendto(message.encode(), address)
+            print("Sending message: " + message + " to drone: " + DRONES[i]["ID"])
+        except Exception as e:
+            print("Error sending: " + message + " to drone: " + DRONES[i]["ID"])
+        
+        time.sleep(delay)
+
+def receive():
+    while True:
+        for i in len(SOCKETS):
+            socket = SOCKETS[i]
+            address = LOCAL_ADDRESSES[i]
+            try:
+                data, addr = socket.recvfrom(1024)
+                print("Received message: " + data.decode() + " from drone: " + DRONES[i]["ID"])
+            except Exception as e:
+                socket.close()
+                print("Error receiving from drone: " + DRONES[i]["ID"])
+                break
+
+receiveThread = threading.Thread(target=receive)
+receiveThread.daemon = True
+receiveThread.start()
 
 """
 END OF SETUP SECTION
@@ -98,3 +129,13 @@ END OF SETUP SECTION
 START OF SWARM CONTROL
 (note everything will be repeated across all drones)
 """
+
+# TODO
+
+"""
+END OF SWARM CONTROL
+
+START OF SHUTDOWN SECTION
+"""
+for socket in SOCKETS:
+    socket.close()
